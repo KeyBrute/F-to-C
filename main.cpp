ab070177a2cdef81b880eddbf9566e1ad2af9050
KeyBrute <mickeymadrigal@yahoo.com>
2022-06-13 20:09:17 -0500

    hw

--- main.cpp
+++ main.cpp
@@ -6,31 +6,20 @@ using namespace std;
 
 int main()
 {
-     int inputC;
-     cout << "Enter a value (Celsius to Fahrenheit): ";
-     cin >> inputC;
-     cout << "Celsius" << setw(15) << "Fahrenheit"
-          << "\n"
-          << endl;
+     int inputEquation;
 
-     for (int i = inputC; i <= inputC + 20; i++)
+     cout << "Enter a value (CtoF and FtoC): ";
+     cin >> inputEquation;
+     cout << "Number" << setw(15) << "CtoF" << setw(15) << "FtoC"
+          << "\n";
+     cout << "----------------------------------------------------"
+          << "\n"
+          << "\n";
+
+     for (int i = inputEquation; i <= inputEquation + 20; i++)
      {
 
-          cout << i << setw(15) << setprecision(2) << fixed << i * (9.0 / 5.0) + 32.0 << "\n"
-               << endl;
-     }
-
-     int inputF;
-     cout << "Enter a value (Fahrenheit to Celsius): ";
-     cin >> inputF;
-     cout << "Fahrenheit" << setw(15) << "Celsius"
-          << "\n"
-          << endl;
-
-     for (int i = inputF; i <= inputF + 20; i++)
-     {
-
-          cout << i << setw(15) << setprecision(2) << fixed << (i - 32.0) * 5.0 / 9.0 << "\n"
+          cout << i << setw(15) << setprecision(2) << fixed << i * (9.0 / 5.0) + 32.0 << setw(15) << setprecision(2) << fixed << (i - 32.0) * 5.0 / 9.0 << "\n"
                << endl;
      }
 
